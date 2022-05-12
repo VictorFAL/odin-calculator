f772e9d7245189bf969ea2b0f2fdcccd0b6cf4e7
Victor | Harry <victorfalmeida12@gmail.com>
2022-05-12 14:28:37 -0300

    buttons added

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function mult(num1, num2) {
 function div(num1, num2) {
     return num1 / num2;
 }
+
+function operate(op, num1, num2) {
+    switch(op) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return sub(num1, num2);
+            break;
+        case '*':
+            return mult(num1, num2);
+            break;
+        case '/':
+            return div(num1, num2);
+            break;
+    }
+}

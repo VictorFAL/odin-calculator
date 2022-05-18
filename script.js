de3aded88c4b61e5a51d9865a3f19dd1b2e485b1
Victor | Harry <victorfalmeida12@gmail.com>
2022-05-18 16:23:51 -0300

    operations completed

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function operate(op, num1, num2) {
         case '-':
             return sub(num1, num2);
             break;
-        case '*':
+        case 'X':
             return mult(num1, num2);
             break;
         case '/':
@@ -31,11 +31,44 @@ function operate(op, num1, num2) {
     }
 }
 
-const display = document.getElementById('display');
-const btns = document.querySelectorAll('.nums');
 
+const display = document.getElementById('display-txt');
+const btns = document.querySelectorAll('.nums');
+const ops = document.querySelectorAll('.operators');
+
+// Populate display by pressing number buttons
 btns.forEach((btn) => {
     btn.addEventListener('click', () => {
         display.innerText += btn.innerText;
     });
 });
+
+
+// Store values by pressing operator buttons
+let values = [];
+
+ops.forEach((op) => {
+    op.addEventListener('click', () => {
+        values.push(op.innerText);
+        values.push(parseFloat(display.innerText));
+        display.innerText = '';
+    });
+});
+
+
+// Run operation by pressing '='
+const btnEqual = document.getElementById('equal');
+
+btnEqual.addEventListener('click', () => {
+    values.push(parseFloat(display.innerText));
+
+    let result = operate(values[0], values[1], values[2]);
+    display.innerText = String(result);
+
+    values = [];
+});
+
+
+// TODO: Clear values by pressing 'AC'
+
+// TODO: Delete last digit by pressing 'DEL'
